30f6fd92057604b84346ab99364aacb645b76e93
unknown <bhavyom19@gmail.com>
2020-01-13 01:19:54 +0530

    End of section 9 Signed-off-by: Bhavyom <bhavyom19@gmail.com>

---
+++ DatingApp-SPA/src/app/_models/photo.ts
@@ -0,0 +1,6 @@
+export interface Photo {
+    id: string;
+    url: string;
+    description: string;
+    isMain: boolean;
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { JwtModule } from '@auth0/angular-jwt';
+import { RouterModule } from '@angular/router';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -11,12 +14,24 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceotroProvider } from './_services/error.interceptor';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
-import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
+import {UserService} from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
-
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
+export class CustomHammerConfig extends HammerGestureConfig {
+   overrides = {
+      pinch: {enable: false},
+      rotate: {enable: false}
+   };
+}
 @NgModule({
    declarations: [
       AppComponent,
@@ -25,19 +40,33 @@ import { appRoutes } from './routes';
       RegisterComponent,
       ListsComponent,
       MessagesComponent,
-      MemberListComponent
-
+      MemberListComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      NgxGalleryModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      TabsModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains: ['localhost:63624'],
+            blacklistedRoutes: ['localhost:63624/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
-      ErrorInterceotroProvider
+      ErrorInterceotroProvider,
+      UserService,
+      MemberDetailResolver,
+      MemberListResolver,
+      {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
    ],
    bootstrap: [
       AppComponent
